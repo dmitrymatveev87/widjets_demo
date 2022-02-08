7895917d19a5b98d5a2971e22b5ae89e7419950e
dmitrymatveev87 <dmitrymatveev87@gmail.com>
2022-02-08 22:20:19 +0300

    Добавлены виджеты с сохранием и загрузкой данных

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:widjets_demo/screens/app_bar_screen.dart';
+import 'package:widjets_demo/screens/files_demo_screen.dart';
 import 'package:widjets_demo/screens/listview_screen_dart.dart';
 import 'package:widjets_demo/screens/navigation_demo_dart.dart';
 import 'package:widjets_demo/screens/navigation_drawer_screen.dart';
+import 'package:widjets_demo/screens/shared_pref_screen.dart';
 import 'package:widjets_demo/screens/switches_demo_screen.dart';
 import 'package:widjets_demo/screens/text_demo_screen.dart';
 import 'package:widjets_demo/screens/theme_demo_screen.dart';
 
 void main() {
-  runApp(const ListViewScreen());
+  runApp(const SharedPrefScreen());
 }
-
