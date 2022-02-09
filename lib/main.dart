e8c8fc1ce38191dc451c3d60f49b6011c135da46
dmitrymatveev87 <dmitrymatveev87@gmail.com>
2022-02-09 22:55:01 +0300

    Отображение данных из сети

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,12 @@ import 'package:widjets_demo/screens/files_demo_screen.dart';
 import 'package:widjets_demo/screens/listview_screen_dart.dart';
 import 'package:widjets_demo/screens/navigation_demo_dart.dart';
 import 'package:widjets_demo/screens/navigation_drawer_screen.dart';
+import 'package:widjets_demo/screens/networking_screen.dart';
 import 'package:widjets_demo/screens/shared_pref_screen.dart';
 import 'package:widjets_demo/screens/switches_demo_screen.dart';
 import 'package:widjets_demo/screens/text_demo_screen.dart';
 import 'package:widjets_demo/screens/theme_demo_screen.dart';
 
 void main() {
-  runApp( const NavigationDemo());
+  runApp( const NetworkingScreen());
 }
